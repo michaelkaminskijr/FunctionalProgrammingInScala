c138d46df41de204ba7635231e1b3ed26d905084
Michael Kaminski <michaelkaminskijr@gmail.com>
2023-08-14 10:23:07 -0500

    Adds exercises

--- src/main/scala/chapter2/main.scala
+++ src/main/scala/chapter2/main.scala
@@ -14,12 +14,13 @@ def fib(n: Int): Int = {
 def isSorted[A](as: Array[A], gt: (A, A) => Boolean): Boolean = {
   @annotation.tailrec
   def loop(n: Int): Boolean = {
-    if (n > as.length-2) true
-    else if (!gt(as(n+1), as(n))) false
+    if n > as.length-2 then true
+    else if !gt(as(n+1), as(n)) then false
     else loop(n+1)
   }
   loop(0)
 }
+
 val result1 = isSorted(Array(1, 2, 3), _ > _)
 val result2 = isSorted(Array(1, 2, 1), _ > _)
 val result3 = isSorted(Array(3, 2, 1), _ < _)
