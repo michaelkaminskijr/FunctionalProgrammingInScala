fd842e0210e07392bc012d8cd0477d4f10c4eea0
Michael Kaminski <michaelkaminskijr@gmail.com>
2023-08-14 20:55:30 -0500

    Adds solution for Exercise 2.5

--- src/main/scala/chapter2/main.scala
+++ src/main/scala/chapter2/main.scala
@@ -37,3 +37,6 @@ def doubler(n: Int): Int =
 def uncurry[A, B, C](f: A => B => C): (A, B) => C =
   (a: A, b: B) => f(a)(b)
 
+// Exercise 2.5
+def compose[A, B, C](f: B => C, g: A => B): A => C =
+  (a: A) => f(g(a))
